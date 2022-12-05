8670f314d7b914130a95149411e1be1f672325b0
Ax <attigmohammed@gmail.com>
2022-12-05 15:28:57 +0100

    home component & template

--- src/app/crud/home/home.component.ts
+++ src/app/crud/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../crud/crud.service';
+import { Product } from '../crud/product';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  private products: Product[] = [];
 
-  constructor() { }
+  constructor(private crudService: CrudService) {}
 
   ngOnInit(): void {
+    this.crudService.getAll().subscribe((data: Product[]) => {
+      this.products = data;
+    });
   }
 
+  delete(id: any) {
+    return this.crudService.delete(id);
+  }
 }
